9a5e3e81290ba3b7898ba4bf11d1da099c289ab4
cafe51 <cafecafe51@hotmail.com>
2022-02-14 02:51:00 -0400

    desafio 4 corrigido

--- src/challenges.js
+++ src/challenges.js
@@ -29,22 +29,94 @@ function splitSentence(string) {
   return arrayR;
 }
 
-console.log(splitSentence('olá amigo'));
-
 // Desafio 4
-function concatName() {
+function concatName(arrayLista) {
   // seu código aqui
+  let stringR = '';.
+  stringR = stringR.concat(arrayLista[arrayLista.length - 1]);
+  stringR = stringR.concat(', ');
+  stringR = stringR.concat(arrayLista[0]);
+  return stringR;
 }
+let exemplo = ['Lucas', 'Cassiano', 'Ferraz', 'Paolillo'];
+
+console.log(concatName(exemplo));
 
 // Desafio 5
-function footballPoints() {
+function footballPoints(wins, ties) {
   // seu código aqui
+  let wPoints = wins * 3;
+  let points = wPoints + ties;
+  return points;
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+
+function highestCount(barray) {
+  // seu código aqui{
+      for (let bindex2 = 1; bindex2 < barray.length; bindex2 += 1) {
+        for (let secondbIndex = 0; secondbIndex < bindex2; secondbIndex += 1) {
+          if (barray[bindex2] > barray[secondbIndex]) {
+            let position = barray[bindex2];
+            barray[bindex2] = barray[secondbIndex];
+            barray[secondbIndex] = position;
+      }
+    }
+  }
+  let arrayContador = [];
+  let counterrr = 0;
+  arrayContador.length = barray.length;
+  for (let indx = 0; indx < barray.length; indx += 1) {
+    for (let indx2 = 0; indx2 < barray.length; indx2 += 1) {
+      if (barray[indx] === barray[indx2]) {
+        counterrr += 1;
+      }
+    }
+    arrayContador[indx] = counterrr;
+    counterrr = 0;
+  }
+  return arrayContador[0];
 }
+
+let x = [0, 4, 4, 4, 9, 2, 1];
+
+console.log(highestCount(x));
+
+
+
+// let arrayContador = [];
+// let counterrr = 0;
+// arrayContador.length = barray.length;
+// for (let indx = 0; indx < barray.length; indx += 1) {
+//   for (let indx2 = 0; indx2 < barray.length; indx2 += 1) {
+//     if (barray[indx] === barray[indx2]) {
+//       counterrr += 1;
+//     }
+//   }
+//   arrayContador[indx] = counterrr;
+//   counterrr = 0;
+// }
+// return arrayContador[0];
+
+
+// function bobblersort(barray) {
+//   for (let bindex2 = 1; bindex2 < barray.length; bindex2 += 1) {
+//     for (let secondbIndex = 0; secondbIndex < bindex2; secondbIndex += 1) {
+//       if (barray[bindex2] > barray[secondbIndex]) {
+//         let position = barray[bindex2];
+//         barray[bindex2] = barray[secondbIndex];
+//         barray[secondbIndex] = position;
+//       }
+//     }
+//   }
+//   return barray;
+// }
+
+
+
+
+
+
 
 // Desafio 7
 function catAndMouse() {
